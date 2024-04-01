e0d6c79ac96ae9ff1ab2469b427de9a388841391
Daniel Floresca <dfloresca@gmail.com>
2024-03-31 20:34:52 -0700

    feat Setup main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,49 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
 import App from './App.jsx'
+import Record from "./components/Record";
+import RecordList from "./components/RecordList";
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBroswerRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <RecordList />,
+      }
+    ]
+  },
+  {
+    path: "/:id/edit",
+    element: <App />,
+    children: [
+      {
+        path: "/:id/edit",
+        element: <Record />,
+      },
+    ],
+  },
+  {
+    path: "/create",
+    element: <App />,
+    children: [
+      {
+        path: "/create",
+        element: <Record />,
+      },
+    ],
+  },
+])
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
